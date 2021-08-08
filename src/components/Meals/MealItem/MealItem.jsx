809afcbe49cd394308f37de65134492304fea597
Madhin <madhinhussain@gmail.com>
2021-08-08 17:49:09 +0100

    modal component added

--- src/components/Meals/MealItem/MealItem.jsx
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -7,7 +7,7 @@ const MealItem = (props) => {
   return (
     <li className = {styles.meal}>
       <div>
-        <h3>{props.name}</h3>
+        <h3 className = {styles.name}>{props.name}</h3>
         <div className = {styles.description}>{props.description}</div>
         <div className = {styles.price}>{price}</div>
       </div>
