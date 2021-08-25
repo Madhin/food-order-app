32fdbfc93e504fcd7f07f4b9f320d0a91057b31a
Madhin <madhinhussain@gmail.com>
2021-08-25 15:35:51 +0100

    menu added

--- src/components/Meals/AvailableMeals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals/AvailableMeals.jsx
@@ -3,41 +3,19 @@ import styles from './AvailableMeals.module.scss'
 import Card from '../../UI/Card/Card';
 import MealItem from '../MealItem/MealItem';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Chicken Tikka Biryani',
-    description: 'Traditional festive dish prepared with basmati rice in clarified better with delicate herbs and spices. Served with vegetable curry.',
-    price: 11.95,
-  },
-  {
-    id: 'm2',
-    name: "Hussain's Bombay Special",
-    description: "Chicken or Lamb Tikka cooked with minced lamb, finely chopped onion, capsicum, spices, fresh herbs and potato (medium to hot)",
-    price: 8.50,
-  },
-  {
-    id: 'm3',
-    name: "Hussain's BBQ Pack",
-    description: "Includes: 2x Tandoori Chicken (on the bone), 4x Chicken Tikka, 4x Lamb Chops, 4x Lamb Tikka, 4x Sheek Kebabs & 2x Naan's", 
-    price: 29.95,
-  },
-  {
-    id: 'm4',
-    name: "Lamb Shanks",
-    description: "Marinated lamb shanks slowly cooked overnight, in tomatoes, onion, fresh herbs and spices, served with mushroom and potatoes",
-    price: 12.25,
-  },
-];
 
 const AvailableMeals = () => {
 
   const [meals, setMeals] = useState([]);
   const [loading, setLoading] = useState(true)
+  const [error, setError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch('https://hussains-46374-default-rtdb.firebaseio.com/Menu.json');
+      if (!response.ok) {
+        throw new Error('Something went wrong!')
+      }
       const responseData = await response.json();
       console.log(responseData)
       const loadedMeals = [];
@@ -53,8 +31,11 @@ const AvailableMeals = () => {
       setMeals(loadedMeals);
       setLoading(false);
     };
-
-    fetchMeals();
+  
+      fetchMeals().catch ((error) => {
+      setLoading(false);
+      setError(error.message)
+      });
   }, []);
 
   console.log(meals)
@@ -64,6 +45,12 @@ const AvailableMeals = () => {
       <p>Loading...</p>
     </section>
     );
+  }
+
+  if (error) {
+    return <section className = {styles.error}>
+    <p>Failed to fetch</p>
+  </section>
   }
 
   const mealsList = meals.map((meal) => <MealItem 
