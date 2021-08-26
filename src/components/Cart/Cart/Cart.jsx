422c605f39d533fd35f5d80ea3ff5055d6859a48
Madhin <madhinhussain@gmail.com>
2021-08-26 12:07:05 +0100

    user details sent to firebase

--- src/components/Cart/Cart/Cart.jsx
+++ src/components/Cart/Cart/Cart.jsx
@@ -8,6 +8,8 @@ import CartItem from './CartItem'
 
 const Cart = (props) => {
   const [checkout, setCheckout] = useState(false)
+  const [submitting, setSubmitting] = useState(false);
+  const [didSubmit, setDidSubmit] = useState(false);
   const cartCtx = useContext(CartContext)
   const cartItemRemoveHandler = id => {cartCtx.removeItem(id)};
   const cartItemAddHandler = item => {cartCtx.addItem({...item, amount: 1})};
@@ -15,10 +17,24 @@ const Cart = (props) => {
   const orderHandler = () => {
     setCheckout(true)
   }
-
   const cancelHandler = () => {
     setCheckout(false)
   }
+
+  const submitOrderHandler = async (userData) => {
+    setSubmitting(true);
+    const response = await fetch('https://hussains-46374-default-rtdb.firebaseio.com/orders.json', {
+      method: 'POST',
+      body: JSON.stringify({
+        user:userData,
+        orderedItems: cartCtx.items
+      })
+    });
+    // setSubmitting(false);
+    setDidSubmit(true);
+  }; 
+
+
   const totalAmount = `£${cartCtx.totalAmount.toFixed(2)}`
   const hasItems = cartCtx.items.length > 0;
   const modalAction = <div className = {styles.actions}>
@@ -26,21 +42,34 @@ const Cart = (props) => {
   {hasItems && <button className = {styles.button} onClick={orderHandler}>Order</button>}
 </div>
 
-  
-  return (
+  const cartModalContent = 
+  <> 
+  {cartItems}
+  <div className = {styles.total}>
+    <span>
+      Total Amount
+    </span>
+    <span>
+      {totalAmount}
+    </span>
+  </div>
+  {checkout && <Checkout onSubmit = {submitOrderHandler} onCancel = {cancelHandler}/>}
+  {!checkout && modalAction}
+</>
+
+const isSubmittingContent = <p>Sending order data...</p>
+
+const didSubmitContent = <>
+<p>Successfully sent order!</p>
+<div className = {styles.actions}>
+  <button className = {styles.button} onClick = {props.onClose}>Close</button>
+  </div>
+</>
+
+ return (
     <Modal onClose = {props.onClose}>
-      {cartItems}
-      <div className = {styles.total}>
-        <span>
-          Total Amount
-        </span>
-        <span>
-          {totalAmount}
-        </span>
-      </div>
-      {checkout && <Checkout onCancel = {cancelHandler}/>}
-      {!checkout && modalAction}
-      
+     {!submitting && cartModalContent}
+     {didSubmit && didSubmitContent}
     </Modal>
   )
 }
--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -11,11 +11,44 @@ const Checkout = (props) => {
     postcode: true
   });
 
- 
+  const nameInputRef = useRef();
+  const roadInputRef = useRef();
+  const cityInputRef = useRef();
+  const postcodeInputRef = useRef();
+
   const confirmHandler = (e) => {
     e.preventDefault();
+    const enteredName = nameInputRef.current.value;
+    const enteredRoad = roadInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
+    const enteredPostcode = postcodeInputRef.current.value;
+
+    const enteredNameIsValid = !isEmpty(enteredName)
+    const enteredRoadIsValid = !isEmpty(enteredRoad)
+    const enteredCityIsValid = !isEmpty(enteredCity)
+    const enteredPostcodeIsValid = !isEmpty(enteredPostcode)
+
+    setFormInputValid({
+      name: enteredNameIsValid,
+      road: enteredRoadIsValid,
+      city: enteredCityIsValid,
+      postcode: enteredPostcodeIsValid
+    })
+
+    const formIsValid = enteredNameIsValid && 
+    enteredRoadIsValid && enteredCityIsValid && 
+    enteredPostcodeIsValid
+
+    if (!formIsValid) {
+      return 
     }
-  
+      props.onSubmit({
+        name: enteredName,
+        road: enteredRoad,
+        city: enteredCity,
+        postcode: enteredPostcode
+      });
+  };
   
   return (
       <form onSubmit={confirmHandler} >
@@ -23,25 +56,25 @@ const Checkout = (props) => {
         <label htmlFor ='name'>
           Your Name
         </label>
-        <input type = 'text' id= 'name' required/>
+        <input type = 'text' id= 'name' ref = {nameInputRef} required/>
         </div>
         <div className = {styles.control}>
         <label htmlFor ='road'>
           Road Name
         </label>
-        <input type = 'text' id= 'road'  required />
+        <input type = 'text' id= 'road' ref = {roadInputRef} required />
         </div>
         <div className = {styles.control}>
         <label htmlFor ='city'>
           City
         </label>
-        <input type = 'text' id= 'city' required/>
+        <input type = 'text' id= 'city' ref = {cityInputRef} required/>
         </div>
         <div className = {styles.control}>
         <label htmlFor ='postcode'>
           Postcode
         </label>
-        <input type = 'text' id= 'postcode' required/>
+        <input type = 'text' id= 'postcode' ref = {postcodeInputRef} required/>
         </div>
         <div className = {styles.actions}>
         <button type ='button' onClick={props.onCancel}>Cancel</button>
